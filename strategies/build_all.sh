a9c8df67e5db1baf3b0c5edac0edd0b6433d02c6
Steffen Zeile <steffen@zeilemail.de>
2025-03-05 18:36:28 +0100

    Update test strategies

--- strategies/build_all.sh
+++ strategies/build_all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-for strategy in */; do
-    if [ -d "$strategy" ]; then
-        cd "$strategy"
-        docker build -t "pd-${strategy%/}" .
-        cd ..
+set -e
+
+BASE_DIR="$(dirname "$0")"
+
+for dir_path in "$BASE_DIR"/*/; do
+    if [ -d "$dir_path" ]; then
+        dir_name=$(basename "$dir_path")
+        docker build --tag "${dir_name}" "$dir_path"
     fi
 done
